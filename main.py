cc96b1581195f90f0f33e022e2a01a25eb584d4e
Kacper Krej <k.krej@ibe.edu.pl>
2023-02-14 10:15:13 +0100

    added movement animation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sim import LinearSlope, QuadraticSlope
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 
 if __name__ == '__main__':
@@ -22,14 +23,28 @@ if __name__ == '__main__':
     t, x, y = sim.simulate(initial_conditions, params)
 
     # plot results
+    # that's rather crude
     fig, axs = plt.subplots(1,3)
-    axs[0].plot(t,x)
-    axs[0].set_xlabel('t')
-    axs[0].set_ylabel('x')
-    axs[1].plot(t,y)
-    axs[1].set_xlabel('t')
-    axs[1].set_ylabel('y')
-    axs[2].plot(x,y)
-    axs[2].set_xlabel('x')
-    axs[2].set_ylabel('y')
-    plt.show()    
+    t_plt = []
+    x_plt = []
+    y_plt = []
+    def animate(i):
+        t_plt.append(t[i])
+        x_plt.append(x[i])
+        y_plt.append(y[i])
+        axs[0].clear()
+        axs[0].plot(t_plt, x_plt)
+        axs[0].set_xlabel('t')
+        axs[0].set_ylabel('x')
+        axs[1].clear()
+        axs[1].plot(t_plt, y_plt)
+        axs[1].set_xlabel('t')
+        axs[1].set_ylabel('y')
+        axs[2].clear()
+        axs[2].plot(x,y)
+        axs[2].plot(x[i],y[i],'o')
+        axs[2].set_xlabel('x')
+        axs[2].set_ylabel('y')
+
+    ani = FuncAnimation(plt.gcf(), animate, frames=len(x), interval=100, repeat=False)
+    plt.show()
